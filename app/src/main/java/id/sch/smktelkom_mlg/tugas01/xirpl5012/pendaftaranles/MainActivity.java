6f2d8966e1b102d497a0215b79d858277a6ff826
GABS <galbie_elinour_24rpl@student.smktelkom-mlg.sch.id>
2016-09-10 21:29:31 +0700

    Validasi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5012/pendaftaranles/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5012/pendaftaranles/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     EditText Nama,Umur;
     Button Daftar;
     TextView Hasil;
+    RadioButton rbPr,rbLk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         Umur = (EditText) findViewById(R.id.editTextUmur);
         Daftar = (Button) findViewById(R.id.buttonDaftar);
         Hasil = (TextView) findViewById(R.id.textViewHasil);
+        rbPr = (RadioButton) findViewById(R.id.radioButtonPr);
+        rbLk = (RadioButton) findViewById(R.id.radioButtonLk);
 
         Daftar.setOnClickListener(new View.OnClickListener()
         {
@@ -30,18 +34,48 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view)
             {
                 doProcess();
-
+                doClick();
             }
         });
+
+    }
+
+    private void doClick() {
+
+
+
     }
 
     private void doProcess() {
+        String hasil = null;
+
+        if (rbPr.isChecked())
+        {
+            hasil = rbPr.getText().toString();
+        }
+
+        else if (rbLk.isChecked());
+        {
+            hasil = rbLk.getText().toString();
+        }
+
+        if (hasil == null)
+        {
+            Hasil.setText("Belum memilih Jenis Kelamin");
+        }
+
+        else
+        {
+            Hasil.setText(" dengan jenis kelamin "+hasil);
+        }
+
         if (isValid())
         {
             String nama = Nama.getText().toString();
             int umur = Integer.parseInt(Umur.getText().toString());
-            Hasil.setText(nama+" diterima dengan usia "+umur+"tahun");
+            Hasil.setText(nama+" diterima dengan usia "+umur+" tahun");
         }
+
     }
 
     private boolean isValid() {
@@ -74,9 +108,9 @@ public class MainActivity extends AppCompatActivity {
         valid = false;
         }
 
-        else if(umur.length()!=2)
+        else if(umur.length() >13 && umur.length()<25)
         {
-            Umur.setError("Format bukan yy");
+            Umur.setError("umur min 14th dan max 24th");
             valid = false;
         }
 
